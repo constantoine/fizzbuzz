05488373946c41c5aee1f09ce1a884038e72be87
Cleo Rebert <cleo.rebert@gmail.com>
2021-09-15 10:44:19 +0200

    FizzBuzz route

--- internal/router.go
+++ internal/router.go
@@ -3,12 +3,31 @@ package internal
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/constantoine/fizzbuzz/pkg"
 )
 
 func RouteFizzBuzz(w http.ResponseWriter, r *http.Request) {
-	res, err := pkg.FizzBuzzWithStats(nil)
+	fizzNum, err := strconv.Atoi(r.Form.Get("int1"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	buzzNum, err := strconv.Atoi(r.Form.Get("int2"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	limit, err := strconv.Atoi(r.Form.Get("limit"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	fizzStr := r.Form.Get("str1")
+	buzzStr := r.Form.Get("str2")
+	req := pkg.NewRequest(fizzNum, buzzNum, fizzStr, buzzStr, limit)
+	res, err := pkg.FizzBuzzWithStats(req)
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
